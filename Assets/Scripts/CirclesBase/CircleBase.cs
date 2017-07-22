579f0c0618d57012bfee0275891a5c054ab10a88
Efimenko Alexander <sashok494949@yandex.ua>
2017-07-22 12:35:52 +0300

    1.0.2

---
+++ Assets/Scripts/CirclesBase/CircleBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CircleBase : MonoBehaviour {
+
+    protected int mass;
+    protected string type;
+    protected GameObject prefabObject;
+
+    protected abstract void CreateObj();
+}
---
+++ Assets/Scripts/CirclesBase/DeathCircle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using UnityEngine;
+
+class DeathCircle : CircleBase
+{
+    private Coroutine coroutineForInst;
+
+    protected override void CreateObj()
+    {
+        coroutineForInst = StartCoroutine(Create());
+    }
+
+    IEnumerator Create()
+    {
+        yield return new WaitForSeconds(2f);
+        Instantiate(prefabObject, prefabObject.transform.position, Quaternion.identity);
+        Repeat();
+    }
+
+    void Repeat()
+    {
+        coroutineForInst= StartCoroutine(Create());
+    }
+}
+
